79a02e3fa8764f49d4b7a1bfd97ef9086cda0cd9
Neill Bogie <neillbogie@googlemail.com>
2024-06-26 22:47:46 +0100

    docs for the helper HangmanGamePage

--- src/test/java/com/hangman/test/HangmanGamePage.java
+++ src/test/java/com/hangman/test/HangmanGamePage.java
@@ -5,10 +5,9 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selectors.byAttribute;
 import static com.codeborne.selenide.Selenide.$;
 
+//A place to declare some elements for convenience.  Not actually needed for the test unless you want to use it.
 public class HangmanGamePage
 {
-    public SelenideElement letterButtonX = $(byAttribute("data-testid", "letter-button-x"));
-
     public SelenideElement letterBoard = $(byAttribute("data-testid", "letter-board"));
 
     public SelenideElement missCount = $(byAttribute("data-testid", "miss-count"));
@@ -16,4 +15,10 @@ public class HangmanGamePage
     public SelenideElement winMessage = $(byAttribute("data-testid", "win-message"));
 
     public SelenideElement lossMessage = $(byAttribute("data-testid", "loss-message"));
+
+    public SelenideElement letterButtonM = $(byAttribute("data-testid", "letter-button-m"));
+    public SelenideElement letterButtonI = $(byAttribute("data-testid", "letter-button-i"));
+    public SelenideElement letterButtonP = $(byAttribute("data-testid", "letter-button-p"));
+    public SelenideElement letterButtonS = $(byAttribute("data-testid", "letter-button-s"));
+    public SelenideElement letterButtonX = $(byAttribute("data-testid", "letter-button-x"));
 }

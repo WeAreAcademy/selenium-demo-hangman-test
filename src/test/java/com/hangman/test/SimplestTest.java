8943f001dc377bf04f0ccbb6d5471971c55d17ca
Neill Bogie <neillbogie@googlemail.com>
2024-06-28 12:52:37 +0100

    improved the given hangman tests with docs, corrections

--- src/test/java/com/hangman/test/SimplestTest.java
+++ src/test/java/com/hangman/test/SimplestTest.java
@@ -1,67 +1,49 @@
 package com.hangman.test;
 
 import com.codeborne.selenide.Configuration;
-//import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
+// There are many ways to work with Selenium.
+// This example uses "Selenide" to make the tests easy to write.
+// The documentation for Selenide library is here:
+// https://selenide.org/documentation.html
 public class SimplestTest {
 
     @BeforeEach
     public void setUp() {
-        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
-        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
         Configuration.browserSize = "1280x800";
-
-        //open the browser automatically to this url.
-        open("https://hangman-game-solution.netlify.app/?testWord=mississippi");
     }
 
     @Test
     public void playGameToVictory() {
-
-        //This automates the playing of a game of hangman from start to finish, winning.
+        //This test automates the playing of a game of hangman from start to finish, winning.
 
         //pseudocode:
-        //number of misses should start at zero
-        //game over shouldn't be present
-        //letterBoard should display _ _ _ _ _ _ _ _ _ _ _  for mississippi.
-        //click x
-        //number of misses should increment
-        // letterBoard shouldn't change
-        //click i
-        //letterBoard should show the 4 is in mississippi, and underscores for the rest
-        // number of guesses should not have incremented
-        //i should now be disabled
-        //click m
-        //click s
-        // game should not be over
-        //click p
-        // win message should be present
-        // loss message should not be present
-
+        // open the browser at the hangman game, forcing it to use the target word "mississippi"
+        open("https://hangman-game-solution.netlify.app/?testWord=mississippi");
 
         // Verify initial state
-        $("[data-testid='miss-count']").shouldHave(text("0"));
+        $("[data-testid='miss-count']").shouldHave(exactText("0"));
         $("[data-testid='loss-message']").shouldNotBe(visible);
         $("[data-testid='win-message']").shouldNotBe(visible);
-        $("[data-testid='letter-board']").shouldHave(matchText("_ _ _ _ _ _ _ _ _ _ _"));
+        $("[data-testid='letter-board']").shouldHave(exactText("_ _ _ _ _ _ _ _ _ _ _"));
 
         // Click 'X' - this will be a miss
         $("[data-testid='letter-button-x']").click();
 
         // Verify miss-count increments
-        $("[data-testid='miss-count']").shouldHave(text("1"));
+        $("[data-testid='miss-count']").shouldHave(exactText("1"));
 
         // Click 'I'
         $("[data-testid='letter-button-i']").click();
 
         // Verify letter board updates and 'I' is disabled
-        $("[data-testid='letter-board']").shouldHave(text("_ i _ _ i _ _ i"));
+        $("[data-testid='letter-board']").shouldHave(exactText("_ i _ _ i _ _ i _ _ i"));
         $("[data-testid='letter-button-i']").shouldBe(disabled);
 
         // Click 'M' and 'S'
@@ -77,6 +59,22 @@ public class SimplestTest {
         // Verify win message and no loss message
         $("[data-testid='win-message']").shouldBe(visible);
         $("[data-testid='loss-message']").shouldNotBe(visible);
-        $("[data-testid='letter-board']").shouldHave(text("m i s s i s s i p p i"));
+        $("[data-testid='letter-board']").shouldHave(exactText("m i s s i s s i p p i"));
+    }
+
+    @Test
+    public void playGameToLossYouWriteThisTest() {
+        //Task for the reader: design and write a test which plays the hangman game and loses.
+        //The test must be completely reliable -
+        // i.e. it should not have ANY possibility of winning (not, say, 1 in 10,000 times)
+
+        //open the browser automatically to this url.
+        open("https://hangman-game-solution.netlify.app/?testWord=potato");
+
+        //This test will fail - we don't really expect miss-count to be 99 at the start of a game!
+        //Delete this line once you've designed your test and are ready to start writing it
+        $("[data-testid='miss-count']").shouldHave(exactText("99"));
+
+        //You have to write the rest here
     }
 }
